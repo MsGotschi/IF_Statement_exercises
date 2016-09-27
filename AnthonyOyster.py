cafce3cafada9b8a4132aa730bed2e4446191f38
Elitennn <anthonymasvikepi@hotmail.co.uk>
2016-09-27 11:40:02 +0100

    added work by Anthony

---
+++ AnthonyOyster.py
@@ -0,0 +1,12 @@
+#anthony masvikepi
+#27/09/2016
+#oyster card
+
+
+age=int(input("how old are you"))
+city=(input("which city do you live in"))
+if age <=18 and city=="london":
+    print("you are able to have an oyster")
+    
+else:
+    print("you are not able to apply")
