69478de57782dd5426d694ad8cdc278a2f2eec3e
brigdan <kacperdev@gmail.com>
2016-09-27 11:01:45 +0100

    Fixed Oyster Code

--- oystercode.py
+++ oystercode.py
@@ -3,7 +3,7 @@
 #Tuesday 27th september
 #Drop MIT licence here
 age = int(input ("Please enter your age: "))
-location = input ("Where do you live? ")
-if age < 19 and location == "london" or "London":
+location = input("Where do you live? ").lower()
+if age < 19 and location == "london":
     print ("You are eligable for an oyster card")
 
