8ace98c5406a2da5f2fdc14399dc60bd0fa1a4c8
Tina Götschi <tina@adacollege.org.uk>
2016-09-27 11:03:40 +0100

    Update DeFreitas_Miguel_Oyster-Card.py
    Fixed some spelling and added a .lower() method call so that all versions of London LONDON LOnDOn etc are accepted

--- DeFreitas_Miguel_Oyster-Card.py
+++ DeFreitas_Miguel_Oyster-Card.py
@@ -1,7 +1,7 @@
 user_location = input("Where do you live? ")
 user_age = input("How old are you? ")
-if int(user_age)<19 and user_location == "London":
-	print("You are elligable for an oyster!")
+if int(user_age)<19 and user_location.lower() == "london":
+	print("You are eligable for an oyster!")
 else:
-	print("Sorry, you are not elligable for an oyster.")
+	print("Sorry, you are not eligable for an oyster.")
 
